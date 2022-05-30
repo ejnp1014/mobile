8acf26c0838138ad21d6de1573cc699564740cc5
ejnpuni <ejnpuni@gmail.com>
2022-05-30 16:25:30 -0500

    23/05/2022 manejo de Widget

---
+++ lib/view/home_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class HomeView extends StatelessWidget{
+
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255,241,222,222),
+        title: const Text ('Home'),
+      ), // AppBar
+
+      body: const Center(
+        child: Text("CAR IV"),
+      ),//  Center
+  );// Scaffold
+  }
+}
